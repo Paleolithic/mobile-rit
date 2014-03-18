11082e44caf60e3ca15e806e80c63bba920ddaff
Thomas Boyle <tjb2597@rit.edu>
2014-03-18 10:50:45 -0400

    Beginning to add markers

--- index.php
+++ index.php
@@ -16,43 +16,73 @@
     <script type="text/javascript">
     $(document).ready(function() {
         map                = new OpenLayers.Map( "basicMap" );
+        
         var mapnik         = new OpenLayers.Layer.OSM();
+        map.addLayer( mapnik );
+        
+        var size = new OpenLayers.Size(21,25);
+        var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+        var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
+
+        var academicBuilds      = new OpenLayers.Layer.Markers();
+        // academicBuilds.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(0,0),icon));
+        map.addLayer( academicBuilds );
+        
+        var residentialBuilds   = new OpenLayers.Layer.Markers();
+        map.addLayer( residentialBuilds );
+        
+        var academicLots        = new OpenLayers.Layer.Markers();
+        map.addLayer( academicLots );
+        
+        var residentialLots     = new OpenLayers.Layer.Markers();
+        map.addLayer( residentialLots );
+        
+        var visitorParks        = new OpenLayers.Layer.Markers();
+        map.addLayer( visitorParks );
+        
+        var food                = new OpenLayers.Layer.Markers();
+        map.addLayer( food );
+        
+        var shopping            = new OpenLayers.Layer.Markers();
+        map.addLayer( shopping );
+        
+        var bikeRacks           = new OpenLayers.Layer.Markers();
+        map.addLayer( bikeRacks );
+        
+        var busStops            = new OpenLayers.Layer.Markers();
+        map.addLayer( busStops  );
+        
+        var womens              = new OpenLayers.Layer.Markers();
+        map.addLayer( womens );
+        
+        var mens                = new OpenLayers.Layer.Markers();
+        map.addLayer( mens );
+        
+        var atms                = new OpenLayers.Layer.Markers();
+        atms.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(-8645875.0011304,5324914.9987629),icon));
+        atms.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(-8646471.5682495,5324952.0229485),icon.clone()));
+        atms.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(-8646693.1161986,5324753.1672421),icon.clone()));
+        atms.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(-8646762.9844199,5324829.0071062),icon.clone()));
+        atms.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(-8647347.0110891,5324550.72855),icon.clone()));
+        map.addLayer( atms );
+        
+        
+        
         var fromProjection = new OpenLayers.Projection( "EPSG:4326" );   // Transform from WGS 1984
         var toProjection   = new OpenLayers.Projection( "EPSG:900913" ); // to Spherical Mercator Projection
         var position       = new OpenLayers.LonLat( -77.68,43.08 ).transform( fromProjection, toProjection );
         /* var position       = new OpenLayers.LonLat( 13.41,52.52 ).transform( fromProjection, toProjection ); */
         var zoom           = 15; 
-
-        map.addLayer( mapnik );
-
-        var academicBuilds      = new OpenLayers.Layer.Markers();
-        var residentialBuilds   = new OpenLayers.Layer.Markers();
-        var academicLots        = new OpenLayers.Layer.Markers();
-        var residentialLots     = new OpenLayers.Layer.Markers();
-        var visitorParks        = new OpenLayers.Layer.Markers();
-        var food                = new OpenLayers.Layer.Markers();
-        var shopping            = new OpenLayers.Layer.Markers();
-        var bikeRacks           = new OpenLayers.Layer.Markers();
-        var busStops            = new OpenLayers.Layer.Markers();
-        var womens              = new OpenLayers.Layer.Markers();
-        var mens                = new OpenLayers.Layer.Markers();
-        var atms                = new OpenLayers.Layer.Markers();
-        
-        map.addLayer( academicBuilds );
-        map.addLayer( residentialBuilds );
-        map.addLayer( academicLots );
-        map.addLayer( residentialLots );
-        map.addLayer( visitorParks );
-        map.addLayer( food );
-        map.addLayer( shopping );
-        map.addLayer( bikeRacks );
-        map.addLayer( busStops  );
-        map.addLayer( womens );
-        map.addLayer( mens );
-        map.addLayer( atms );
-        
         map.setCenter( position, zoom );
+
+        map.events.register("click", map, function(e){
+            var position = map.getLonLatFromPixel(e.xy);
+            console.log(position);
+        });
     });
+
+
+    
     </script>
 
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,200,800' rel='stylesheet' type='text/css'>
